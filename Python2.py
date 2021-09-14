cd5d6e6a6d112af9841be9c96b4729d6695eed22
Jace <letmejace00@gmail.com>
2021-09-14 18:02:13 +0900

    commit

---
+++ Python2.py
@@ -0,0 +1,16 @@
+# 1
+
+# a = "Life is too short, you need python"
+
+# if "wife" in a: print("wife")
+# elif "python" in a and "you" not in a: print("python")
+# elif "shirt" not in a: print("shirt")
+# elif "need" in a: print("need")
+# else: print("none")
+
+# shirt
+
+# 2
+# while문을 사용해 1부터 1000까지의 자연수 중 3의 배수의 합을 구해 보자.
+
+
